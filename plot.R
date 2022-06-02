54ebf738e87e072f26e164b4abe6ab3d262afa1d
ismael KONE <105424486+kismaelci@users.noreply.github.com>
2022-06-02 17:12:03 +0000

    Update plot.R

--- plot.R
+++ plot.R
@@ -2,12 +2,15 @@
 
 ## read the csv data
 outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+
 ## look at the first few rows
 head(outcome)
+
 ## look at some more informations about dataset
 ncol(outcome)
 nrow(outcome)
 names(outcome)
+
 ## let s set "30-day death rates from heart attack" (column 11 in the outcome dataset) as numeric
 outcome[,11] <- as.numeric(outcome[,11])
 hist(outcome[,11], col = 2, main = "Hospital 30-Day Death (mortality) rates for heart attack", xlab = "Deaths")
